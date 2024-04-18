6ac531ae705584a33bee633d4ce5f409c1485640
Ivanildo Junior <101154477+0dL1n@users.noreply.github.com>
2024-04-18 12:28:45 -0300

    versão melhorada

--- calculadora.cpp
+++ calculadora.cpp
@@ -17,10 +17,16 @@ float distanceEuclidiana(Point p1, Point p2){
 }
 
 int main(){
-	Point p1 = {5.0, 4.0};
-	Point p2 = {3.5, 4.0};
+	Point p1; Point p2;
+	cout << "Calculadora Euclidiana" << endl;
+	cout << "digite as coordenadas (x,y) do ponto P1: ";
+	cin >> p1.x >> p1.y;
+	cout << "digite as coordenadas (x,y) do ponto P2: ";
+	cin >> p2.x >> p2.y;
+	//Point p1 = {5.0, 4.0};
+	//Point p2 = {3.5, 4.0};
 	float distancia = distanceEuclidiana(p1, p2);
-	cout << distancia;
+	cout << "A Distancia Euclidiana entre os pontos P1 e P2 e igual a " << distancia;
 	
 	return 0;
 	
